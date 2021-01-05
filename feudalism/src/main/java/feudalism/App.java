12523bb07e35c312cc67569f844e105e3243d452
Traffic <trafficconegod@gmail.com>
2021-01-05 18:56:43 -0500

    add broken plugin compile process

--- feudalism/src/main/java/feudalism/App.java
+++ feudalism/src/main/java/feudalism/App.java
@@ -1,24 +1,32 @@
 package feudalism;
-
 import java.util.UUID;
+import org.bukkit.plugin.java.JavaPlugin;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        Realm realm = new Realm();
-        realm.setOwner(UUID.randomUUID());
-        Realm r2 = new Realm();
-        r2.setOverlord(realm);
-        r2.removeOverlord();
-        // realm.removeSubject(r2);
-        System.out.println(realm.getSubjects());
-        System.out.println(r2.hasOverlord());
-        System.out.println(r2.getOverlord());
+public class App extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello, SpigotMC!");
+    }
+    
+    @Override
+    public void onDisable() {
+        getLogger().info("See you again, SpigotMC!");
     }
 }
+
+// public class App 
+// {
+//     public static void main( String[] args )
+//     {
+//         System.out.println( "Hello World!" );
+//         Realm realm = new Realm();
+//         realm.setOwner(UUID.randomUUID());
+//         Realm r2 = new Realm();
+//         r2.setOverlord(realm);
+//         r2.removeOverlord();
+//         // realm.removeSubject(r2);
+//         System.out.println(realm.getSubjects());
+//         System.out.println(r2.hasOverlord());
+//         System.out.println(r2.getOverlord());
+//     }
+// }
