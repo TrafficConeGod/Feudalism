7445b59eb1f92e67a854af4ea490e3dc9d28a192
Traffic <trafficconegod@gmail.com>
2021-01-05 19:14:08 -0500

    fix

--- feudalism/src/main/java/feudalism/App.java
+++ feudalism/src/main/java/feudalism/App.java
@@ -7,7 +7,7 @@ public class App extends JavaPlugin {
     public void onEnable() {
         getLogger().info("Hello, SpigotMC!");
     }
-    
+
     @Override
     public void onDisable() {
         getLogger().info("See you again, SpigotMC!");
