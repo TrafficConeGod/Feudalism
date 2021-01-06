82d9fe5f3e0a6236d1a91368b654a192748ade07
Traffic <trafficconegod@gmail.com>
2021-01-06 17:45:13 -0500

    add failed azrael test

--- feudalism/src/main/java/feudalism/App.java
+++ feudalism/src/main/java/feudalism/App.java
@@ -1,8 +1,6 @@
 package feudalism;
 import java.util.UUID;
 import org.bukkit.plugin.java.JavaPlugin;
-// import ca.uqac.lif.azrael.json.JsonPrinter;
-// import ca.uqac.lif.json.JsonElement;
 
 import feudalism.command.realm.RealmCommand;
 import feudalism.object.Realm;
--- feudalism/src/test/java/feudalism/AppTest.java
+++ feudalism/src/test/java/feudalism/AppTest.java
@@ -1,16 +1,23 @@
 package feudalism;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+
+import java.util.UUID;
 
 import org.junit.Test;
 
 import feudalism.object.Realm;
+import ca.uqac.lif.azrael.PrintException;
+import ca.uqac.lif.azrael.ReadException;
+import ca.uqac.lif.azrael.json.JsonPrinter;
+import ca.uqac.lif.azrael.json.JsonReader;
+import ca.uqac.lif.json.JsonElement;
 
-public class AppTest 
-{
+public class AppTest {
     @Test
-    public void overlordSubjectLinkTest()
-    {
+    public void overlordSubjectLinkTest() {
         Realm r1 = new Realm();
         Realm r2 = new Realm();
         r2.setOverlord(r1);
@@ -26,5 +33,25 @@ public class AppTest
         r2.removeOverlord();
         assertEquals(false, r2.hasOverlord());
         assertEquals(false, r1.getSubjects().size() > 0);
+    }
+
+    @Test
+    public void azraelTest() {
+        UUID uuid = UUID.randomUUID();
+        Realm realm = new Realm(uuid);
+        realm.setName("Test");
+        JsonPrinter printer = new JsonPrinter();
+        JsonReader reader = new JsonReader();
+        try {
+            JsonElement elem = printer.print(realm);
+            Realm realmReconstruct = (Realm) reader.read(elem);
+            assertEquals(true, realm.getName() == realmReconstruct.getName());
+        } catch (PrintException e) {
+            e.printStackTrace();
+            assertSame(true, false);
+        } catch (ReadException e) {
+            e.printStackTrace();
+            assertSame(true, false);
+        }
     }
 }
