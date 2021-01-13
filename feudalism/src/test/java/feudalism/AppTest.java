07b41af904290df8fd81d4f43755d85967463e83
Traffic <trafficconegod@gmail.com>
2021-01-12 20:35:15 -0500

    fix

--- feudalism/src/test/java/feudalism/AppTest.java
+++ feudalism/src/test/java/feudalism/AppTest.java
@@ -27,97 +27,97 @@ import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.lib.jse.JsePlatform;
 
 public class AppTest {
-    // @Test
-    // public void realmsTest() throws FeudalismException {
-    //     float upkeepFactor = Config.getFloat("realm.upkeep_factor");
-    //     User user = new User();
-    //     Realm r1 = new Realm();
-    //     r1.addClaim(GridCoord.getFromGridPosition(0, 0));
-    //     r1.addClaim(GridCoord.getFromGridPosition(0, 1));
-    //     r1.setOwner(user);
-    //     Realm r2 = new Realm();
-    //     r1.addClaim(GridCoord.getFromGridPosition(1, 0));
-    //     r1.addClaim(GridCoord.getFromGridPosition(1, 1));
-    //     r2.setOwner(user);
-    //     r2.setOverlord(r1);
-    //     assertEquals(true, r2.getOverlord() == r1);
-    //     assertEquals(true, r1.getSubjects().get(0) == r2);
-    //     r2.removeOverlord();
-    //     assertEquals(false, r2.hasOverlord());
-    //     assertEquals(false, r1.getSubjects().size() > 0);
-    //     assertEquals(true, user.getOwnedRealms().size() == 2);
-    //     assertEquals(true, user.getUpkeep() == upkeepFactor * 4);
-    //     Registry.resetInstance();
-    // }
+    @Test
+    public void realmsTest() throws FeudalismException {
+        float upkeepFactor = Config.getFloat("realm.upkeep_factor");
+        User user = new User();
+        Realm r1 = new Realm();
+        r1.addClaim(GridCoord.getFromGridPosition(0, 0));
+        r1.addClaim(GridCoord.getFromGridPosition(0, 1));
+        r1.setOwner(user);
+        Realm r2 = new Realm();
+        r1.addClaim(GridCoord.getFromGridPosition(1, 0));
+        r1.addClaim(GridCoord.getFromGridPosition(1, 1));
+        r2.setOwner(user);
+        r2.setOverlord(r1);
+        assertEquals(true, r2.getOverlord() == r1);
+        assertEquals(true, r1.getSubjects().get(0) == r2);
+        r2.removeOverlord();
+        assertEquals(false, r2.hasOverlord());
+        assertEquals(false, r1.getSubjects().size() > 0);
+        assertEquals(true, user.getOwnedRealms().size() == 2);
+        assertEquals(true, user.getUpkeep() == upkeepFactor * 4);
+        Registry.resetInstance();
+    }
 
-    // @Test
-    // public void azraelRealmTest() throws PrintException, ReadException, FeudalismException {
-    //     User user = User.get(UUID.randomUUID());
-    //     Realm realm = new Realm();
-    //     realm.setName("Test");
-    //     realm.setOwner(user);
-    //     Realm r2 = new Realm();
-    //     r2.setName("Test2");
-    //     r2.setOverlord(realm);
-    //     JsonPrinter printer = new JsonPrinter();
-    //     JsonReader reader = new JsonReader();
-    //     JsonElement elem = printer.print(realm);
-    //     Realm realmReconstruct = (Realm) reader.read(elem);
-    //     assertEquals(true, realm.getName() == realmReconstruct.getName());
-    //     assertEquals(true, realm.getUuid().equals(realmReconstruct.getUuid()));
-    //     assertEquals(true, realm.getSubjects().size() == realmReconstruct.getSubjects().size());
-    //     assertEquals(true, realm.getSubjects().get(0).getName() == realmReconstruct.getSubjects().get(0).getName());
-    //     assertEquals(true, realm.getOwner().equals(realmReconstruct.getOwner()));
-    //     assertEquals(true, realm.getClaims().size() == realmReconstruct.getClaims().size());
-    //     assertEquals(true, user == realmReconstruct.getOwner());
-    //     Registry.resetInstance();
-    // }
+    @Test
+    public void azraelRealmTest() throws PrintException, ReadException, FeudalismException {
+        User user = User.get(UUID.randomUUID());
+        Realm realm = new Realm();
+        realm.setName("Test");
+        realm.setOwner(user);
+        Realm r2 = new Realm();
+        r2.setName("Test2");
+        r2.setOverlord(realm);
+        JsonPrinter printer = new JsonPrinter();
+        JsonReader reader = new JsonReader();
+        JsonElement elem = printer.print(realm);
+        Realm realmReconstruct = (Realm) reader.read(elem);
+        assertEquals(true, realm.getName() == realmReconstruct.getName());
+        assertEquals(true, realm.getUuid().equals(realmReconstruct.getUuid()));
+        assertEquals(true, realm.getSubjects().size() == realmReconstruct.getSubjects().size());
+        assertEquals(true, realm.getSubjects().get(0).getName() == realmReconstruct.getSubjects().get(0).getName());
+        assertEquals(true, realm.getOwner().equals(realmReconstruct.getOwner()));
+        assertEquals(true, realm.getClaims().size() == realmReconstruct.getClaims().size());
+        assertEquals(true, user == realmReconstruct.getOwner());
+        Registry.resetInstance();
+    }
 
-    // @Test
-    // public void azraelGridCoordTest() throws PrintException, ReadException, FeudalismException {
-    //     GridCoord coord = GridCoord.getFromGridPosition(100, 100);
-    //     JsonPrinter printer = new JsonPrinter();
-    //     JsonReader reader = new JsonReader();
-    //     JsonElement elem = printer.print(coord);
-    //     GridCoord coordR = (GridCoord) reader.read(elem);
-    //     assertEquals(true, coord.getGridX() == coordR.getGridX());
-    //     assertEquals(true, coord.getGridZ() == coordR.getGridZ());
-    //     Registry.resetInstance();
-    // }
+    @Test
+    public void azraelGridCoordTest() throws PrintException, ReadException, FeudalismException {
+        GridCoord coord = GridCoord.getFromGridPosition(100, 100);
+        JsonPrinter printer = new JsonPrinter();
+        JsonReader reader = new JsonReader();
+        JsonElement elem = printer.print(coord);
+        GridCoord coordR = (GridCoord) reader.read(elem);
+        assertEquals(true, coord.getGridX() == coordR.getGridX());
+        assertEquals(true, coord.getGridZ() == coordR.getGridZ());
+        Registry.resetInstance();
+    }
 
-    // @Test
-    // public void azraelUserTest() throws PrintException, ReadException, FeudalismException {
-    //     User user = User.get(UUID.randomUUID());
-    //     JsonPrinter printer = new JsonPrinter();
-    //     JsonReader reader = new JsonReader();
-    //     JsonElement elem = printer.print(user);
-    //     User userR = (User) reader.read(elem);
-    //     assertEquals(true, user.getUuid().equals(userR.getUuid()));
-    //     Registry.resetInstance();
-    // }
+    @Test
+    public void azraelUserTest() throws PrintException, ReadException, FeudalismException {
+        User user = User.get(UUID.randomUUID());
+        JsonPrinter printer = new JsonPrinter();
+        JsonReader reader = new JsonReader();
+        JsonElement elem = printer.print(user);
+        User userR = (User) reader.read(elem);
+        assertEquals(true, user.getUuid().equals(userR.getUuid()));
+        Registry.resetInstance();
+    }
 
-    // @Test
-    // public void luaTest() throws FeudalismException {
-    //     Globals globals = JsePlatform.standardGlobals();
-    //     LuaValue val = globals.load("return \"Test string\"").call();
-    //     assertEquals(true, val.equals(LuaValue.valueOf("Test string")));
-    //     Registry.resetInstance();
-    // }
+    @Test
+    public void luaTest() throws FeudalismException {
+        Globals globals = JsePlatform.standardGlobals();
+        LuaValue val = globals.load("return \"Test string\"").call();
+        assertEquals(true, val.equals(LuaValue.valueOf("Test string")));
+        Registry.resetInstance();
+    }
 
-    // @Test
-    // public void gridCoordTest() throws FeudalismException {
-    //     Realm realm = new Realm();
-    //     realm.addClaim(GridCoord.getFromWorldPosition(0, 0));
-    //     realm.addClaim(GridCoord.getFromWorldPosition(0, 1));
-    //     realm.addClaim(GridCoord.getFromWorldPosition(1, 1));
-    //     realm.addClaim(GridCoord.getFromWorldPosition(1, 1));
-    //     realm.addClaim(GridCoord.getFromGridPosition(1, 10));
-    //     assertEquals(true, realm.getClaims().size() == 1);
-    //     int size = Registry.getInstance().getRealms().size();
-    //     realm.removeClaim(GridCoord.getFromGridPosition(0, 0));
-    //     assertEquals(true, Registry.getInstance().getRealms().size() == size - 1);
-    //     Registry.resetInstance();
-    // }
+    @Test
+    public void gridCoordTest() throws FeudalismException {
+        Realm realm = new Realm();
+        realm.addClaim(GridCoord.getFromWorldPosition(0, 0));
+        realm.addClaim(GridCoord.getFromWorldPosition(0, 1));
+        realm.addClaim(GridCoord.getFromWorldPosition(1, 1));
+        realm.addClaim(GridCoord.getFromWorldPosition(1, 1));
+        realm.addClaim(GridCoord.getFromGridPosition(1, 10));
+        assertEquals(true, realm.getClaims().size() == 1);
+        int size = Registry.getInstance().getRealms().size();
+        realm.removeClaim(GridCoord.getFromGridPosition(0, 0));
+        assertEquals(true, Registry.getInstance().getRealms().size() == size - 1);
+        Registry.resetInstance();
+    }
 
     @Test
     public void siegeTest() throws FeudalismException {
@@ -155,82 +155,82 @@ public class AppTest {
         Registry.resetInstance();
     }
 
-    // @Test
-    // public void configTest() throws FeudalismException {
-    //     assertEquals(true, Config.getInt("#siege.goals") == 3);
-    //     Registry.resetInstance();
-    // }
+    @Test
+    public void configTest() throws FeudalismException {
+        assertEquals(true, Config.getInt("#siege.goals") == 3);
+        Registry.resetInstance();
+    }
 
-    // @Test
-    // public void siegeGoalTest() throws FeudalismException {
-    //     assertEquals(true, Registry.getInstance().hasSiegeGoal("subjugate"));
-    //     SiegeGoal goal = Registry.getInstance().getSiegeGoal("subjugate");
-    //     Realm r1 = new Realm();
-    //     Realm r2 = new Realm();
-    //     goal.execute(r1, r2, new ArrayList<>());
-    //     assertEquals(true, r2.getOverlord() == r1);
-    //     Registry.resetInstance();
-    // }
+    @Test
+    public void siegeGoalTest() throws FeudalismException {
+        assertEquals(true, Registry.getInstance().hasSiegeGoal("subjugate"));
+        SiegeGoal goal = Registry.getInstance().getSiegeGoal("subjugate");
+        Realm r1 = new Realm();
+        Realm r2 = new Realm();
+        goal.execute(r1, r2, new ArrayList<>());
+        assertEquals(true, r2.getOverlord() == r1);
+        Registry.resetInstance();
+    }
 
-    // @Test
-    // public void siegeSerializationTest() throws FeudalismException, ReadException, PrintException {
-    //     assertEquals(true, Registry.getInstance().hasSiegeGoal("subjugate"));
-    //     Realm r1 = new Realm();
-    //     r1.setName("R1");
-    //     Realm r2 = new Realm();
-    //     r2.setName("R2");
-    //     Realm ally = new Realm();
-    //     Siege siege = new Siege(r1, r2, Registry.getInstance().getSiegeGoal("subjugate"));
-    //     siege.addAlly(r1, ally);
-    //     JsonPrinter printer = new JsonPrinter();
-    //     JsonReader reader = new JsonReader();
-    //     JsonElement elem = printer.print(siege);
-    //     Siege siegeRec = (Siege) reader.read(elem);
-    //     assertEquals(true, siegeRec.getAttacker() == siege.getAttacker());
-    //     assertEquals(true, siegeRec.getDefender() == siege.getDefender());
-    //     assertEquals(true, siegeRec.getAttackers().size() == siege.getAttackers().size());
-    //     assertEquals(true, siegeRec.getDefenders().size() == siege.getDefenders().size());
-    //     assertEquals(true, siegeRec.getGoal() == siege.getGoal());
-    //     Registry.resetInstance();
-    // }
+    @Test
+    public void siegeSerializationTest() throws FeudalismException, ReadException, PrintException {
+        assertEquals(true, Registry.getInstance().hasSiegeGoal("subjugate"));
+        Realm r1 = new Realm();
+        r1.setName("R1");
+        Realm r2 = new Realm();
+        r2.setName("R2");
+        Realm ally = new Realm();
+        Siege siege = new Siege(r1, r2, Registry.getInstance().getSiegeGoal("subjugate"));
+        siege.addAlly(r1, ally);
+        JsonPrinter printer = new JsonPrinter();
+        JsonReader reader = new JsonReader();
+        JsonElement elem = printer.print(siege);
+        Siege siegeRec = (Siege) reader.read(elem);
+        assertEquals(true, siegeRec.getAttacker() == siege.getAttacker());
+        assertEquals(true, siegeRec.getDefender() == siege.getDefender());
+        assertEquals(true, siegeRec.getAttackers().size() == siege.getAttackers().size());
+        assertEquals(true, siegeRec.getDefenders().size() == siege.getDefenders().size());
+        assertEquals(true, siegeRec.getGoal() == siege.getGoal());
+        Registry.resetInstance();
+    }
 
-    // @Test
-    // public void directBorderTest() throws FeudalismException {
-    //     Realm realm = new Realm();
-    //     realm.addClaim(GridCoord.getFromGridPosition(1, 0));
-    //     GridCoord coord = GridCoord.getFromGridPosition(2, 0);
-    //     GridCoord coordNot = GridCoord.getFromGridPosition(3, 0);
-    //     assertEquals(true, realm.hasDirectBorder(coord));
-    //     assertEquals(false, realm.hasDirectBorder(coordNot));
-    //     Registry.resetInstance();
-    // }
+    @Test
+    public void directBorderTest() throws FeudalismException {
+        Realm realm = new Realm();
+        realm.addClaim(GridCoord.getFromGridPosition(1, 0));
+        GridCoord coord = GridCoord.getFromGridPosition(2, 0);
+        GridCoord coordNot = GridCoord.getFromGridPosition(3, 0);
+        assertEquals(true, realm.hasDirectBorder(coord));
+        assertEquals(false, realm.hasDirectBorder(coordNot));
+        Registry.resetInstance();
+    }
 
-    // @Test
-    // public void borderRadiusTest() throws FeudalismException {
-    //     Realm realm = new Realm();
-    //     realm.addClaim(GridCoord.getFromGridPosition(1, 0));
-    //     GridCoord coord = GridCoord.getFromGridPosition(5, 0);
-    //     GridCoord coordNot = GridCoord.getFromGridPosition(12, 0);
-    //     assertEquals(true, realm.isWithinBorderRadius(coord));
-    //     assertEquals(false, realm.isWithinBorderRadius(coordNot));
-    //     Registry.resetInstance();
-    // }
+    @Test
+    public void borderRadiusTest() throws FeudalismException {
+        Realm realm = new Realm();
+        realm.addClaim(GridCoord.getFromGridPosition(1, 0));
+        GridCoord coord = GridCoord.getFromGridPosition(5, 0);
+        GridCoord coordNot = GridCoord.getFromGridPosition(12, 0);
+        assertEquals(true, realm.isWithinBorderRadius(coord));
+        assertEquals(false, realm.isWithinBorderRadius(coordNot));
+        Registry.resetInstance();
+    }
 
-    // @Test
-    // public void siegeBorderRadiusTest() throws FeudalismException {
-    //     assertEquals(true, Registry.getInstance().hasSiegeGoal("subjugate"));
-    //     Realm r1 = new Realm();
-    //     r1.addClaim(GridCoord.getFromGridPosition(0, 0));
-    //     Realm r2 = new Realm();
-    //     r2.addClaim(GridCoord.getFromGridPosition(50, 10));
-    //     try {
-    //         new Siege(r1, r2, Registry.getInstance().getSiegeGoal("subjugate"));
-    //         assertEquals(true, false);
-    //     } catch (FeudalismException e) {
-    //         assertEquals(true, true);
-    //     }
-    //     Registry.resetInstance();
-    // }
+    @Test
+    public void siegeBorderRadiusTest() throws FeudalismException {
+        assertEquals(true, Registry.getInstance().hasSiegeGoal("subjugate"));
+        Realm r1 = new Realm();
+        r1.addClaim(GridCoord.getFromGridPosition(0, 0));
+        Realm r2 = new Realm();
+        r2.addClaim(GridCoord.getFromGridPosition(50, 10));
+        try {
+            new Siege(r1, r2, Registry.getInstance().getSiegeGoal("subjugate"));
+            assertEquals(true, false);
+        } catch (FeudalismException e) {
+            assertEquals(true, true);
+        }
+        Registry.resetInstance();
+    }
 
     // @Test
     // public void configTest() throws FeudalismException, IOException {
